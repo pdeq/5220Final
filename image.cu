20dd4b239cd14c76f35ac1cc6e82bc2fd38ef030
Michael Avellino <avellm@login04.chn.perlmutter.nersc.gov>
2024-04-28 18:09:59 -0700

    removed p3 code

--- image.cu
+++ image.cu
@@ -4,100 +4,18 @@
 
 #define NUM_THREADS 256
 
-// Put any static global variables here that you will use throughout the simulation.
-int blks;
-
-__device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
-    double dx = neighbor.x - particle.x;
-    double dy = neighbor.y - particle.y;
-    double r2 = dx * dx + dy * dy;
-    if (r2 > cutoff * cutoff)
-        return;
-    // r2 = fmax( r2, min_r*min_r );
-    r2 = (r2 > min_r * min_r) ? r2 : min_r * min_r;
-    double r = sqrt(r2);
-
-    //
-    //  very simple short-range repulsive force
-    //
-    double coef = (1 - cutoff / r) / r2 / mass;
-    particle.ax += coef * dx;
-    particle.ay += coef * dy;
-}
-
-__global__ void compute_forces_gpu(particle_t* particles, int num_parts) {
-    // Get thread (particle) ID
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid >= num_parts)
-        return;
-
-    particles[tid].ax = particles[tid].ay = 0;
-    for (int j = 0; j < num_parts; j++)
-        apply_force_gpu(particles[tid], particles[j]);
-}
-
-__global__ void move_gpu(particle_t* particles, int num_parts, double size) {
-
-    // Get thread (particle) ID
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid >= num_parts)
-        return;
-
-    particle_t* p = &particles[tid];
-    //
-    //  slightly simplified Velocity Verlet integration
-    //  conserves energy better than explicit Euler method
-    //
-    p->vx += p->ax * dt;
-    p->vy += p->ay * dt;
-    p->x += p->vx * dt;
-    p->y += p->vy * dt;
-
-    //
-    //  bounce from walls
-    //
-    while (p->x < 0 || p->x > size) {
-        p->x = p->x < 0 ? -(p->x) : 2 * size - p->x;
-        p->vx = -(p->vx);
-    }
-    while (p->y < 0 || p->y > size) {
-        p->y = p->y < 0 ? -(p->y) : 2 * size - p->y;
-        p->vy = -(p->vy);
-    }
-}
-
-void init_simulation(particle_t* parts, int num_parts, double size) {
-    // You can use this space to initialize data objects that you may need
-    // This function will be called once before the algorithm begins
-    // parts live in GPU memory
-    // Do not do any particle simulation here
-
-    blks = (num_parts + NUM_THREADS - 1) / NUM_THREADS;
-}
-
-void simulate_one_step(particle_t* parts, int num_parts, double size) {
-    // parts live in GPU memory
-    // Rewrite this function
-
-    // Compute forces
-    compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
-
-    // Move particles
-    move_gpu<<<blks, NUM_THREADS>>>(parts, num_parts, size);
-}
-
 // Serial. Parallelize and put on GPU later
-uint8_t *three_kernel(uint8_t *color_array, float *mask, int num_frames, int height, int width){
-    uint8_t *masked_array = (uint8_t *) malloc(num_frames * height * width * sizeof(uint8_t));
+int *three_kernel(int *color_array, float *mask, int num_frames, int height, int width){
+    int *masked_array = (int *) malloc(num_frames * height * width * sizeof(int));
     for (int i = 0; i < num_frames; ++i){
         for (int j = 1; j < height - 1; ++j){ // Avoid edges (at least for now)
             for (int k = 1; k < width - 1; ++k){ // Avoid edges
                 int index = i * (width * height) + j * (width) + k;
-                masked_array[index] = (uint8_t) (mask[4] * color_array[index]);
-                masked_array[index] += (uint8_t) (mask[5] * color_array[index + 1] + mask[3] * color_array[index - 1]); // Horizontal neighbors
-                masked_array[index] += (uint8_t) (mask[7] * color_array[index + width] + mask[1] * color_array[index - width]); // Vertical neighbors
-                masked_array[index] += (uint8_t) (mask[8] * color_array[index + width + 1] + mask[2] * color_array[index - width + 1]); // Right diag
-                masked_array[index] += (uint8_t) (mask[6] * color_array[index + width - 1] + mask[0] * color_array[index - width - 1]); // Left diag
+                masked_array[index] = (int) (mask[4] * color_array[index]);
+                masked_array[index] += (int) (mask[5] * color_array[index + 1] + mask[3] * color_array[index - 1]); // Horizontal neighbors
+                masked_array[index] += (int) (mask[7] * color_array[index + width] + mask[1] * color_array[index - width]); // Vertical neighbors
+                masked_array[index] += (int) (mask[8] * color_array[index + width + 1] + mask[2] * color_array[index - width + 1]); // Right diag
+                masked_array[index] += (int) (mask[6] * color_array[index + width - 1] + mask[0] * color_array[index - width - 1]); // Left diag
             }
         }
     }
@@ -105,17 +23,17 @@ uint8_t *three_kernel(uint8_t *color_array, float *mask, int num_frames, int hei
 }
 
 // Serial. Parallelize and put on GPU later
-void tint_color(uint8_t *color_array, uint8_t color_val, float weight, int array_len){
+void tint_color(int *color_array, int color_val, float weight, int array_len){
     for (int i = 0; i < array_len; ++i){
-        color_array[i] = (uint8_t) ((color_val + weight * color_array[i]) > 255 ? 255 : (color_val + weight * color_array[i]));
+        color_array[i] = (int) ((color_val + weight * color_array[i]) > 255 ? 255 : (color_val + weight * color_array[i]));
 
     }
 }
 
 // Serial. Parallelize and put on GPU later
-void shade_color(uint8_t *color_array, uint8_t color_val, float weight, int array_len){
+void shade_color(int *color_array, int color_val, float weight, int array_len){
     for (int i = 0; i < array_len; ++i){
-        color_array[i] = (uint8_t) ((color_val - weight * color_array[i]) < 0 ? 0 : (color_val - weight * color_array[i]));
+        color_array[i] = (int) ((color_val - weight * color_array[i]) < 0 ? 0 : (color_val - weight * color_array[i]));
 
     }
 }
--- main.cu
+++ main.cu
@@ -15,150 +15,11 @@
 // Helper Functions
 // =================
 
-#define IS_PETER true
+#define IS_PETER false
 std::string MY_PATH;
 std::string GIF_ID;
 
-// I/O routines
-void save(std::ofstream& fsave, particle_t* parts, int num_parts, double size) {
-    static bool first = true;
-
-    if (first) {
-        fsave << num_parts << " " << size << std::endl;
-        first = false;
-    }
-
-    for (int i = 0; i < num_parts; ++i) {
-        fsave << parts[i].x << " " << parts[i].y << std::endl;
-    }
-
-    fsave << std::endl;
-}
-
-// Particle Initialization
-void init_particles(particle_t* parts, int num_parts, double size, int part_seed) {
-    std::random_device rd;
-    std::mt19937 gen(part_seed ? part_seed : rd());
-
-    int sx = (int)ceil(sqrt((double)num_parts));
-    int sy = (num_parts + sx - 1) / sx;
-
-    std::vector<int> shuffle(num_parts);
-    for (int i = 0; i < shuffle.size(); ++i) {
-        shuffle[i] = i;
-    }
-
-    for (int i = 0; i < num_parts; ++i) {
-        // Make sure particles are not spatially sorted
-        std::uniform_int_distribution<int> rand_int(0, num_parts - i - 1);
-        int j = rand_int(gen);
-        int k = shuffle[j];
-        shuffle[j] = shuffle[num_parts - i - 1];
-
-        // Distribute particles evenly to ensure proper spacing
-        parts[i].x = size * (1. + (k % sx)) / (1 + sx);
-        parts[i].y = size * (1. + (k / sx)) / (1 + sy);
-
-        // Assign random velocities within a bound
-        std::uniform_real_distribution<float> rand_real(-1.0, 1.0);
-        parts[i].vx = rand_real(gen);
-        parts[i].vy = rand_real(gen);
-    }
-}
-
-// Command Line Option Processing
-int find_arg_idx(int argc, char** argv, const char* option) {
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], option) == 0) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int find_int_arg(int argc, char** argv, const char* option, int default_value) {
-    int iplace = find_arg_idx(argc, argv, option);
-
-    if (iplace >= 0 && iplace < argc - 1) {
-        return std::stoi(argv[iplace + 1]);
-    }
-
-    return default_value;
-}
-
-char* find_string_option(int argc, char** argv, const char* option, char* default_value) {
-    int iplace = find_arg_idx(argc, argv, option);
-
-    if (iplace >= 0 && iplace < argc - 1) {
-        return argv[iplace + 1];
-    }
-
-    return default_value;
-}
-
-// ==============
-// Main Function
-// ==============
-
-// int main(int argc, char** argv) {
-//     // Parse Args
-//     if (find_arg_idx(argc, argv, "-h") >= 0) {
-//         std::cout << "Options:" << std::endl;
-//         std::cout << "-h: see this help" << std::endl;
-//         std::cout << "-n <int>: set number of particles" << std::endl;
-//         std::cout << "-o <filename>: set the output file name" << std::endl;
-//         std::cout << "-s <int>: set particle initialization seed" << std::endl;
-//         return 0;
-//     }
-
-//     // Open Output File
-//     char* savename = find_string_option(argc, argv, "-o", nullptr);
-//     std::ofstream fsave(savename);
-
-//     // Initialize Particles
-//     int num_parts = find_int_arg(argc, argv, "-n", 1000);
-//     int part_seed = find_int_arg(argc, argv, "-s", 0);
-//     double size = sqrt(density * num_parts);
-
-//     particle_t* parts = new particle_t[num_parts];
-
-//     init_particles(parts, num_parts, size, part_seed);
-
-//     particle_t* parts_gpu;
-//     cudaMalloc((void**)&parts_gpu, num_parts * sizeof(particle_t));
-//     cudaMemcpy(parts_gpu, parts, num_parts * sizeof(particle_t), cudaMemcpyHostToDevice);
-
-//     // Algorithm
-//     auto start_time = std::chrono::steady_clock::now();
-
-//     init_simulation(parts_gpu, num_parts, size);
-
-//     for (int step = 0; step < nsteps; ++step) {
-//         simulate_one_step(parts_gpu, num_parts, size);
-//         cudaDeviceSynchronize();
-
-//         // Save state if necessary
-//         // if (fsave.good() && (step % savefreq) == 0) {
-//         //     cudaMemcpy(parts, parts_gpu, num_parts * sizeof(particle_t), cudaMemcpyDeviceToHost);
-//         //     save(fsave, parts, num_parts, size);
-//         // }
-//     }
-
-//     cudaDeviceSynchronize();
-//     auto end_time = std::chrono::steady_clock::now();
-
-//     std::chrono::duration<double> diff = end_time - start_time;
-//     double seconds = diff.count();
-
-//     // Finalize
-//     std::cout << "Simulation Time = " << seconds << " seconds for " << num_parts << " particles.\n";
-//     fsave.close();
-//     cudaFree(parts_gpu);
-//     delete[] parts;
-// }
-
-
-void output_array(uint8_t* arr, std::string color, int num_frames, int height, int width) {
+void output_array(int* arr, std::string color, int num_frames, int height, int width) {
     std::ofstream file(MY_PATH + GIF_ID + "-modified." + color);
     if (!file.is_open()) std::cerr << "Cannot open output file." << std::endl;
 
@@ -169,9 +30,10 @@ void output_array(uint8_t* arr, std::string color, int num_frames, int height, i
 
     // Add array contents
     const int arr_size = num_frames * height * width;
-    std::cout << arr_size << std::endl;
     for (int i = 0; i < arr_size; ++i) {
         int curr_num = arr[i];
+        curr_num = max(curr_num, 0);
+        curr_num = min(curr_num, 255);
         if (i % width == (width - 1)) {
             file << curr_num << std::endl;
         } else {
@@ -181,9 +43,9 @@ void output_array(uint8_t* arr, std::string color, int num_frames, int height, i
     file.close();
 }
 
-uint8_t* three_kernel(uint8_t *color_array, float *mask, int num_frames, int height, int width);
-void tint_color(uint8_t *color_array, uint8_t color_val, float weight, int array_len);
-void shade_color(uint8_t *color_array, uint8_t color_val, float weight, int array_len);
+int* three_kernel(int *color_array, float *mask, int num_frames, int height, int width);
+void tint_color(int *color_array, int color_val, float weight, int array_len);
+void shade_color(int *color_array, int color_val, float weight, int array_len);
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -204,9 +66,9 @@ int main(int argc, char** argv) {
     r_file.ignore(1);
     r_file >> width;
 
-    uint8_t *red_array = (uint8_t*) malloc(num_frames * height * width * sizeof(uint8_t));
-    uint8_t *green_array = (uint8_t*) malloc(num_frames * height * width * sizeof(uint8_t));
-    uint8_t *blue_array = (uint8_t*) malloc(num_frames * height * width * sizeof(uint8_t));
+    int *red_array = (int*) malloc(num_frames * height * width * sizeof(int));
+    int *green_array = (int*) malloc(num_frames * height * width * sizeof(int));
+    int *blue_array = (int*) malloc(num_frames * height * width * sizeof(int));
 
 
     std::string word;
@@ -238,20 +100,23 @@ int main(int argc, char** argv) {
     b_file.close();
 
 
-    // uint8_t* red_gpu, green_gpu, blue_bpu;
-    // cudaMalloc((void**)&red_gpu, num_frames * height * width * sizeof(uint8_t));
-    // cudaMalloc((void**)&green_gpu, num_frames * height * width * sizeof(uint8_t));
-    // cudaMalloc((void**)&blue_gpu, num_frames * height * width * sizeof(uint8_t));
+    // int* red_gpu, green_gpu, blue_bpu;
+    // cudaMalloc((void**)&red_gpu, num_frames * height * width * sizeof(int));
+    // cudaMalloc((void**)&green_gpu, num_frames * height * width * sizeof(int));
+    // cudaMalloc((void**)&blue_gpu, num_frames * height * width * sizeof(int));
 
-    // cudaMemcpy(red_gpu, red_array, num_frames * height * width * sizeof(uint8_t), cudaMemcpyHostToDevice);
-    // cudaMemcpy(green_gpu, green_array, num_frames * height * width * sizeof(uint8_t), cudaMemcpyHostToDevice);
-    // cudaMemcpy(blue_gpu, blue_array, num_frames * height * width * sizeof(uint8_t), cudaMemcpyHostToDevice);
+    // cudaMemcpy(red_gpu, red_array, num_frames * height * width * sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(green_gpu, green_array, num_frames * height * width * sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(blue_gpu, blue_array, num_frames * height * width * sizeof(int), cudaMemcpyHostToDevice);
 
     // float mask[] = {0.0, -1.0, 0.0, -1.0, 0.5, -1.0, 0.0, -1.0, 0.0};
     // red_array = three_kernel(red_array, mask, num_frames, height, width);
     // green_array = three_kernel(green_array, mask, num_frames, height, width);
     // blue_array = three_kernel(blue_array, mask, num_frames, height, width);
 
+    tint_color(red_array, 120, 0.9, num_frames * height * width);
+    tint_color(green_array, 200, 0.3, num_frames * height * width);
+    tint_color(blue_array, 90, 0.1, num_frames * height * width);
 
     output_array(red_array, "red", num_frames, height, width);
     output_array(green_array, "green", num_frames, height, width);
